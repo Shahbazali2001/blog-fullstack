10c595ac9365942aeb9edf47bf05c0622805e48e
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-02-26 13:17:11 +0500

    DB Services Updated

--- src/appwrite/config_service.js
+++ src/appwrite/config_service.js
@@ -2,3 +2,99 @@ import config from '../config/config.js';
 
 import { Client, ID, Databases, Storage, Query } from 'appwrite';
  
+export class BlogService {
+    client = new Client();
+    databases;
+    bucket;
+
+
+    constructor(){
+        this.client
+        .setEndpoint(config.appwrite_Url)
+        .setProject(config.appwrite_ProjectId)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(th is.client)     
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        
+        try{
+            return await this.databases.createDocument(
+                config.appwrite_DatabaseId,
+                config.appwrite_CollectionId,
+                slug,
+                {title, content, featuredImage, status, userId,}
+            )
+
+        }catch(error){
+            throw new Error(error.message);
+        }
+    }
+
+    async updatePost(slug, {title, slug, content, featuredImage, status }){
+        try{
+            return await this.databases.updateDocument(
+                config.appwrite_DatabaseId,
+                config.appwrite_CollectionId,
+                slug,
+                {title, slug, content, featuredImage, status }
+            )
+
+        }catch(error){
+            throw new Error(error.message);
+    
+        }    
+    }
+
+    async deletePost(slug){ 
+        try{
+
+            await this.databases.deleteDocument(
+                config.appwrite_DatabaseId,
+                config.appwrite_CollectionId,
+                slug
+            )
+            return true; 
+
+            }catch(error){
+                throw new Error('Post not found');
+                return false;
+            }
+    }
+
+
+    async getPost(){
+        try{
+            return await this.databases.getDocument(
+                config.appwrite_DatabaseId,
+                config.appwrite_CollectionId,
+                slug
+            )
+
+        }catch(error){
+            throw new Error(error.message);
+            return null;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try{
+            return await this.databases.listDocuments(
+                config.appwrite_DatabaseId,
+                config.appwrite_CollectionId,
+                queries,
+
+            )
+        }catch(error){
+            throw new Error(error.message); 
+            return null;
+            
+     }
+    }
+
+    //
+
+}
+
+const Service = new Service();
+export default Service;
