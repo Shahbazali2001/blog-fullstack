86087c0ddb34762515e2fae4973ba9750876db9b
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-15 14:40:18 +0500

    Minor Updates

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,16 @@
 import { forwardRef, useId } from "react"
 
-const Input = forwardRef( function Input({
+const Input = forwardRef( function Input(
+    {
     label,
     type = "text",
     className = "",
     ...props
-},  ref) {
+    },  ref) {
 
 const id = useId();
+
+
 return (
     <div className="w-full">
         {label && <label htmlFor={id} className="inline-block mb-1 pl-1">{label}</label>}
